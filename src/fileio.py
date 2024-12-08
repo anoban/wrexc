bb2be586b7107c9852a7849433d8bba5f94bd48d
anoban <2016sb117@univ.jfn.ac.lk>
2024-12-08 08:11:12 +0530

    modified:   src/fileio.py

--- src/fileio.py
+++ src/fileio.py
@@ -1,7 +1,5 @@
 def open_preprocessed_source_file(file_path: str) -> str:
-    """
-
-    """
+    """ """
     with open(file=file_path, mode="r", encoding="ascii") as file_handle:
         text: str = file_handle.read()
 
